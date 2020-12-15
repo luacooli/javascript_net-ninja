7a4799d028f2efbc33ce227eaa5f907e2e4fcd1a
luana2813 <luanadez13@gmail.com>
2020-12-15 07:13:20 -0300

    feat: functions vs methods

---
+++ chapter_4/functionsXmethods.js
@@ -0,0 +1,11 @@
+const name = 'shaun';
+
+// functions
+const greet3 = () => 'hello';
+
+let resultOne = greet3();
+console.log(resultOne);
+
+// methods
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
