3f46f3008271d9adc75e3cdf530f64ce8c77da5d
luana2813 <luanadez13@gmail.com>
2021-01-11 06:46:51 -0300

    feat: click event done

--- chapter_6/click_events.js
+++ chapter_6/click_events.js
@@ -1,25 +1,35 @@
-const ul = document.querySelector('ul');
-const button = document.querySelector('button');
+const ul = document.querySelector("ul");
+const button = document.querySelector("button");
 
-button.addEventListener('click', () => {
+button.addEventListener("click", () => {
   // ul.innerHTML += '<li>something new</li>';
 
-  const li = document.createElement('li');
-  li.textContent = 'something new todo';
+  const li = document.createElement("li");
+  li.textContent = "something new todo";
   // ul.append(li);
   ul.prepend(li);
 });
 
-const items = document.querySelectorAll('li');
+// const items = document.querySelectorAll("li");
 
-items.forEach(item => {
-  item.addEventListener('click', e => {
-    // console.log('item clicked');
-    // console.log(e)
-    // console.log(e.target)
-    // console.log(item)
+// items.forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     // console.log('item clicked');
+//     // console.log(e)
+//     // console.log(e.target)
+//     // console.log(item)
 
-    // e.target.style.textDecoration = 'line-through';
-    e.target.remove()
-  });
+//     // e.target.style.textDecoration = 'line-through';
+//     console.log("event li");
+//     e.stopPropagation();
+
+//     e.target.remove();
+//   });
+// });
+
+ul.addEventListener("click", (e) => {
+  // console.log('event ul');
+  if (e.target.tagName === 'LI') {
+    e.target.remove();
+  }
 });
