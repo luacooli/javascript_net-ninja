46cc71b13a706103e6f607aab9afc9f9411ee3ba
luana2813 <luanadez13@gmail.com>
2021-01-18 06:42:03 -0300

    feat: username validation

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -1,27 +1,35 @@
 const form = document.querySelector('.signup-form');
-// const username = document.querySelector('#username');
+const feedback = document.querySelector('.feedback');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
 
-  // console.log(username.value);
-  console.log(form.username.value);
+  // validation
+  const username = form.username.value;
+  const usernamePattern = /^[a-zA-Z]{6,12}$/;
+
+  if (usernamePattern.test(username)) {
+    // feedback good info
+    feedback.textContent = 'that username is valid!';
+  } else {
+    feedback.textContent = 'username must contain letters only & be between 6 & 12 characters long';
+  }
 });
 
 // testing RegEx
 
-const username = 'shaunp';
-const pattern = /^[a-z]{6,}/;
+// const username = 'shaunp';
+// const pattern = /^[a-z]{6,}/;
 
 
-// let result = pattern.test(username);
+// // let result = pattern.test(username);
+// // console.log(result);
+
+// // if (result) {
+// //   console.log('regex test passed :)');
+// // } else {
+// //   console.log('regex test failed :(');
+// // }
+
+// let result = username.search(pattern);
 // console.log(result);
-
-// if (result) {
-//   console.log('regex test passed :)');
-// } else {
-//   console.log('regex test failed :(');
-// }
-
-let result = username.search(pattern);
-console.log(result);
