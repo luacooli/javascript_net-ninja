452cae752e67b475213ce57cfb1abd6bbd64f0c7
luana2813 <luanadez13@gmail.com>
2021-02-08 06:55:27 -0300

    feat: add and delete item

--- chapter_10_todos/app.js
+++ chapter_10_todos/app.js
@@ -1,8 +1,42 @@
-const addForm = document.querySelector('.add');
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+const search = document.querySelector('.search input');
 
-addForm.addEventListener('submit', e => {
+const generateTemplate = (todo) => {
+  const html = `
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${todo}</span>
+      <i class="far fa-trash-alt delete"></i>
+    </li>
+  `;
+
+  list.innerHTML += html;
+};
+
+addForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const todo = addForm.add.value;
-  console.log(todo);
+  const todo = addForm.add.value.trim();
+
+  if (todo.length) {
+    generateTemplate(todo);
+    addForm.reset();
+  }
+});
+
+// delete todos
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
+
+const filterTodos = term => {
+  Array.from(list.children)
+}
+
+// keyup event
+search.addEventListener('keyup', () => {
+  const term = search.value.trim();
+  filterTodos(term);
 })
