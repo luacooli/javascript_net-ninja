260d71d3973d1f3f8ed4d69b534f7e1307a5d4d4
luana2813 <luanadez13@gmail.com>
2021-03-18 07:03:49 -0300

    feat: async & await

--- chapter_12/fetchAPI.js
+++ chapter_12/fetchAPI.js
@@ -1,13 +1,31 @@
+// async & await
+
+const getTodos = async () => {
+  const response = await fetch("todos/luigi.json");
+
+  if (response.status !== 200) {
+    throw new Error("cannot fecth the data");
+  }
+
+  const data = await response.json();
+
+  return data;
+};
+
+getTodos()
+  .then((data) => console.log("resolved:", data))
+  .catch((err) => console.log("rejected:", err.message));
+
 // fetch api
 
-fetch("todos/luigi.json")
-  .then((response) => {
-    console.log("resolved", response);
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log("rejected", err);
-  });
+// fetch("todos/luigi.json")
+//   .then((response) => {
+//     console.log("resolved", response);
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log("rejected", err);
+//   });
