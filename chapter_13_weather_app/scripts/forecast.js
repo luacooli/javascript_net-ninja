8f653abb5564e80f86fcbb71f57c73fdb6b33d26
luana2813 <luanadez13@gmail.com>
2021-03-23 07:02:48 -0300

    feat: fetch weather api

---
+++ chapter_13_weather_app/scripts/forecast.js
@@ -0,0 +1,15 @@
+const key = "6E1QyqEDGql5Vjcjwi5GowZWZ6aGwQbA";
+
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity("manchester")
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
