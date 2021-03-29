4ee8d3dc13da81d17b964a70a1ef806eaa811485
luana2813 <luanadez13@gmail.com>
2021-03-29 06:57:24 -0300

    feat: day/night time

--- chapter_13_weather_app/scripts/app.js
+++ chapter_13_weather_app/scripts/app.js
@@ -1,10 +1,12 @@
 const cityForm = document.querySelector("form");
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 const updateUI = (data) => {
-  const cityDetails = data.cityDetails;
-  const weather = data.weather;
+  // destructure properties
+  const { cityDetails, weather } = data;
 
   // update details template
   details.innerHTML = `
@@ -16,9 +18,19 @@ const updateUI = (data) => {
     </div>
   `;
 
+  // ipdate the day/night & icon images
+  let timeSrc = null;
+  if (weather.IsDayTime) {
+    timeSrc = "img/day.svg";
+  } else {
+    timeSrc = "img/night.svg";
+  }
+
+  time.setAttribute("src", timeSrc);
+
   // remove tha d-none class if present
-  if (card.classList.contains('d-none')) {
-    card.classList.remove('d-none');
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
   }
 };
 
