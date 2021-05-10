ac04edb7bd4839a151f27d4787bf760c8518cc88
luana2813 <luanadez13@gmail.com>
2021-05-10 07:01:59 -0300

    feat: real time update and unsubscribe

--- chapter_16/sandbox.js
+++ chapter_16/sandbox.js
@@ -1,5 +1,6 @@
 const list = document.querySelector("ul");
 const form = document.querySelector("form");
+const button = document.querySelector("button");
 
 const addRecipe = (recipe, id) => {
   let time = recipe.created_at.toDate();
@@ -14,16 +15,28 @@ const addRecipe = (recipe, id) => {
   list.innerHTML += html;
 };
 
+const deleteRecipe = (id) => {
+  const recipes = document.querySelectorAll("li");
+
+  recipes.forEach((recipe) => {
+    if (recipe.getAttribute("data-id") === id) {
+      recipe.remove();
+    }
+  });
+};
+
 // get documents
-db.collection("recipes")
-  .get()
-  .then((snapshot) => {
-    // when we have the data
-    snapshot.docs.forEach((doc) => {
+const unsub = db.collection("recipes").onSnapshot((snapshot) => {
+  snapshot.docChanges().forEach((change) => {
+    const doc = change.doc;
+
+    if (change.type === "added") {
       addRecipe(doc.data(), doc.id);
-    });
-  })
-  .catch((err) => console.log(err));
+    } else if (change.type === "removed") {
+      deleteRecipe(doc.id);
+    }
+  });
+});
 
 // add documents
 form.addEventListener("submit", (e) => {
@@ -51,4 +64,11 @@ list.addEventListener("click", (e) => {
       .then(() => console.log("recipe deleted"))
       .catch((err) => console.log(err));
   }
+});
+
+// unsubcribe from database changes
+button.addEventListener("click", () => {
+  unsub();
+
+  console.log("unsubscribed from collection changes");
 });
