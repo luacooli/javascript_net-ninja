5dadccb562f565f47440e8968ef7a9c73cb9ba2d
luana2813 <luanadez13@gmail.com>
2021-05-24 08:39:53 -0300

    feat: update room and username

--- chapter_17_chat/scripts/chat.js
+++ chapter_17_chat/scripts/chat.js
@@ -8,6 +8,7 @@ class Chatroom {
     this.room = room;
     this.username = username;
     this.chats = db.collection("chats");
+    this.unsub;
   }
   async addChat(message) {
     // format a chat object
@@ -24,11 +25,11 @@ class Chatroom {
     return response;
   }
   getChats(callback) {
-    this.chats
+    this.unsub = this.chats
       .where("room", "==", this.room)
       .orderBy("created_at")
-      .onSnapshot((snapshot) => {
-        snapshot.docChanges().forEach((change) => {
+      .onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
           if (change.type === "added") {
             // update the ui
             callback(change.doc.data());
@@ -36,8 +37,27 @@ class Chatroom {
         });
       });
   }
+  updateName(username) {
+    this.username = username;
+  }
+  updateRoom(room) {
+    this.room = room;
+    console.log("room updated");
+    if (this.unsub) {
+      this.unsub();
+    }
+  }
 }
 
-const chatroom = new Chatroom("gaming", "shaun");
+const chatroom = new Chatroom("general", "shaun");
 
-chatroom.getChats((data) => console.log(data));
+chatroom.getChats(data => console.log(data));
+
+setTimeout(() => {
+  chatroom.updateRoom("gaming");
+  chatroom.updateName("yoshi");
+  chatroom.getChats(data => {
+    console.log(data);
+  });
+  chatroom.addChat("hello");
+}, 3000);
