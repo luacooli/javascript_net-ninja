eafe1f544d3da879596dec4c3309859e2882be78
luana2813 <luanadez13@gmail.com>
2021-07-02 09:56:57 -0300

    feat: set

--- chapter_18/sandbox.js
+++ chapter_18/sandbox.js
@@ -17,3 +17,35 @@ console.log(members);
 const person = { name: "shaun", age: 30, job: "net ninja" };
 const personClone = { ...person, location: "manchester" };
 console.log(personClone);
+
+const namesArray = ["ryu", "chun-li", "ryu", "shaun"];
+console.log(namesArray);
+
+// const namesSet = new Set(namesArray);
+// console.log(namesSet);
+
+// const uniqueNames = [...namesSet];
+const uniqueNames = [...new Set(namesArray)];
+console.log(uniqueNames);
+
+const ages = new Set();
+ages.add(20);
+ages.add(25).add(30);
+ages.add(25);
+ages.delete(25);
+
+console.log(ages, ages.size);
+console.log(ages.has(20), ages.has(25));
+
+ages.clear();
+console.log(ages);
+
+const ninja = new Set([
+  { name: "shaun", age: 30 },
+  { name: "crystal", age: 29 },
+  { name: "chun-li", age: 32 },
+]);
+
+ninja.forEach(ninja => {
+  console.log(ninja.name, ninja.age);
+});
