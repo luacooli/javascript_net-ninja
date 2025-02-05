520dc5add3eb393e2982fdf66b1881b168092867
Luana Correia <luacooli@gmail.com>
2025-02-05 11:12:58 -0300

    feat: tabs and snackbar

--- chapter_20_ui-library/src/index.js
+++ chapter_20_ui-library/src/index.js
@@ -1,5 +1,7 @@
 import Tooltip from "./ninja-ui/tooltip"
 import Accordion from "./ninja-ui/accordion"
+import Tabs from "./ninja-ui/tabs"
+import Snackbar from "./ninja-ui/snackbar"
 
 // create tooltip
 const tooltip = new Tooltip(document.querySelector('.tooltip'))
@@ -10,4 +12,17 @@ const accordions = document.querySelectorAll('.accordion')
 accordions.forEach(accordion => {
   const instance = new Accordion(accordion)
   instance.init()
+})
+
+// create tabs
+const tabs = new Tabs(document.querySelector('.tabs'))
+tabs.init()
+
+// create snackbar
+const snackbar = new Snackbar
+snackbar.init()
+
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+  snackbar.show('You clicked me 😉')
 })
